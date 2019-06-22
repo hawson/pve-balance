30cd881e29915ed1a87dfb3b7c372e4e6ac88caf
Jesse Becker <beckerj@ibbr.umd.edu>
2019-06-22 15:30:51 -0400

    move scoring into separate file

--- balance.py
+++ balance.py
@@ -5,28 +5,18 @@ import re
 import sys
 import json
 
+from score import *
 
 from proxmoxer import ProxmoxAPI
+
+
 proxmox = ProxmoxAPI('pve3.ad.ibbr.umd.edu', user='monitoring@pve', password='monitoring',)
+
+
 
 
 nodes = {}
 vms = {}
-
-weight = {
-    'mem':  1.0,
-    'disk': 0.0,   # This will be normalized to GB (not bytes
-    'cpu':  1.0,   # CPU utilization as "fraction_as_percentage",
-    'net':  1.0,   # This will be normalized to MB/sec (not Mbps)
-    'ncpu': 1/4.0, # Scale down total CPU count, bias more towards usage
-}
-
-# these values are added directly to the compute score of each VM/Node
-global_bias = {
-    'pve1': 10,
-    'bcmd7': -10,
-}
-
 
 #
 # https://pve.proxmox.com/pve-docs/api-viewer/index.html
@@ -39,7 +29,7 @@ def get_stats_node(proxmox, exclude=[]):
             nodes[node['node']] = node
         else:
             print("excluding {}".format(node['node']))
-        
+
 
     return nodes
 
@@ -71,7 +61,7 @@ def get_stats_vm(proxmox, exclude=[]):
             vms[vm['vmid']] = vm
         else:
             print("excluding {}/{}".format(vm['vmid'],vm['name']))
-        
+
     #for vm in vms:
     #    print("{0} {1} => {2}".format(vm, vms[vm]['name'], human_format(vms[vm]['maxmem'])))
 
@@ -104,65 +94,6 @@ def human_format(num,precision=1):
     format_str = '%.' + str(precision) + 'f%s'
     #return '%.2f%s' % (num, ['', 'K', 'M', 'G', 'T', 'P'][magnitude])
     return format_str % (num, ['', 'K', 'M', 'G', 'T', 'P'][magnitude])
-
-
-def score_vm(vm, biased):
-    bias_score = 0.0
-    name = vm['name']
-    if biased:
-        if name in global_bias:
-            bias_score = global_bias[vm['name']]
-
-    return (vm['cpu']                  * weight['cpu']   # percentage of allocated CPU used
-            +vm['maxcpu']              * weight['ncpu']  # number of CPUs
-            +vm['mem']/vm['maxmem']    * weight['mem']   # Calculate % RAM used
-            +vm['maxdisk']             * weight['disk'] / 1073741824
-            #+(vm['netin']+vm['netout']) * weight['net'] / 1048576 +
-            + bias_score
-           )
-
-def score_node(node, loadout, mode='total', biased=True, output='short'):
-    '''How we generate a "score" for a node, given a specific loadout of VMs'''
-    score = 0.0
-
-    node_score = 0.0
-    vm_score   = 0.0
-    node_bias  = 0.0
-    vm_bias    = 0.0
-    
-    node_name = node['node']
-
-    if mode == 'total' or mode == 'node':
-        # metrics we care about
-        cpu    = node['cpu']
-        maxcpu = node['maxcpu']
-        maxmem = node['maxmem']
-        mem    = node['mem']
-        #net    = node['netin'] + node['netout']
-        #disk   = node['diskread'] + node['diskwrite']
-
-
-        node_score = (cpu/maxcpu * weight['cpu'] +
-                      mem/maxmem * weight['mem'])
-
-    if mode == 'total' or mode == 'vm':
-        for vmid, vm in loadout.items():
-            if vm['node'] != node_name:
-                continue
-
-            vm_score += score_vm(vm, biased)
-
-
-    # calculate bias
-    if biased:
-        if node_name in global_bias:
-            node_bias = global_bias[node_name]
-
-    score = node_score + vm_score + node_bias + vm_bias
-    if output=='full':
-        return '{:6.3f} = {:5.3f} + {:5.3f} + {:4.1f} + {:4.1f}'.format(score, node_score, vm_score, node_bias, vm_bias)
-    return human_format(score,precision=3)
-
 
 
 
