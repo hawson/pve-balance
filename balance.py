8f4a8c85ca86cba2f5534c428836ad697415b041
Jesse Becker <beckerj@ibbr.umd.edu>
2019-06-22 21:17:44 -0400

    Add balance_math

--- balance.py
+++ balance.py
@@ -6,6 +6,7 @@ import sys
 import json
 
 from score import *
+from balance_math import *
 
 from proxmoxer import ProxmoxAPI
 
---
+++ balance_math.py
@@ -0,0 +1,22 @@
+# My silly implementation of some basic math routines
+
+from math import sqrt
+from operator import mul
+from functools import reduce
+
+# dot product of two vector of arbitrary, but equal length
+def dot(a,b):
+    return sum(map(mul, a, b))
+
+
+def length(a):
+    return  sqrt(sum(map(lambda x: x**2, a)))
+
+def norm(a):
+    l = length(a)
+    return list(map(lambda x: x/l, a))
+
+def area(a):
+    return reduce(lambda acc, x: acc*x, a)
+
+
