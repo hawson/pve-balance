322477eb9f589376d32527a126517e9965832e7a
Jesse Becker <hawson@gmail.com>
2019-07-07 23:41:45 -0400

    move common packing setup code to pack_setup(), add pack_size_rr() and skeleton code

--- packing.py
+++ packing.py
@@ -42,9 +42,33 @@ transitions. :)'''
 # iteration of placement.
 
 import logging
-from balance_math import *
 
 log = logging.getLogger(__name__)
+
+def pack_setup(orig_nodes, orig_vms, vm_sort_key='area'):
+
+    nodes = sorted(orig_nodes.copy(), key=lambda n: n.area(), reverse=True)
+    logging.debug("Sorted node order (by area): {}".format(list(map(str,nodes))))
+
+    for node in nodes:
+        node.allocated_vms = []
+
+    method = None
+    try:
+        sort_method = getattr(orig_vms[0], vm_sort_key)
+    except AttributeError:
+        raise NotImplementedError("The vm class does not have {} a method".format(vm_sort_key))
+
+    log.info("Found method {}".format(vm_sort_key))
+
+    vms = orig_vms.copy()
+    vms.sort(key=lambda v: getattr(v, vm_sort_key)(), reverse=True)
+
+    logging.debug("Sorted VM order (by {}): {}".format(vm_sort_key, list(map(str,vms))))
+
+
+    return nodes, vms
+
 
 def pack_size(orig_nodes, orig_vms, key='area'):
     '''A naive packing routine that only allocates by "size" of
@@ -54,47 +78,27 @@ def pack_size(orig_nodes, orig_vms, key='area'):
 
     log.info("Packing by size")
 
-    vm_metrics = {}
+    nodes, vms = pack_setup(orig_nodes, orig_vms, vm_sort_key=key)
 
-    # sort by the total "area"
-    vms = orig_vms.copy()
-
-    nodes = sorted(orig_nodes.copy(), key=lambda n: n.area(), reverse=True)
-
-    for node in nodes:
-        node.allocated_vms = []
-
-
-    if   key == 'score':
-        log.info("Sorting by score.")
-        vms.sort(key=lambda v: v.score(), reverse=True)
-
-    elif key == 'area':
-        log.info("Sorting by area.")
-        vms.sort(key=lambda v: v.area(), reverse=True)
-
-    elif key == 'area_perc':
-        log.info("Sorting by area_perc.")
-        vms.sort(key=lambda v: v.area_perc(), reverse=True)
+#    vm_metrics = {}
+#
+#    for vm in vms:
+#        metrics = {}
+#
+#        # scale RAM to GB
+#        metrics['area'] = vm.area()
+#        metrics['area_perc'] = vm.area_perc()
+#        metrics['score'] = vm.score()
+#
+#        vm_metrics[vm.vmid] = metrics
 
 
-    for vm in vms:
-        metrics = {}
-
-        # scale RAM to GB
-        metrics['area'] = vm.area()
-        metrics['area_perc'] = vm.area_perc()
-        metrics['score'] = vm.score()
-
-        vm_metrics[vm.vmid] = metrics
-
+    allocated_vms = []
 
     #List is already sorted, so fill up as much as possible.
-    allocated_vms = []
     while vms:
 
         allocations = 0
-
 
         for vm in vms:
             #print(list(map(str,vms)))
@@ -139,13 +143,70 @@ def pack_size(orig_nodes, orig_vms, key='area'):
     return nodes, len(allocated_vms), len(vms)
 
 
-# a slightly less naive packing routine that only allocates "size",
-# looping over all nodes at once, so long as there is capacity in
-# any node.
-def pack_size_rr(orig_nodes, vms):
+def pack_size_rr(orig_nodes, orig_vms, key='area'):
+    '''a slightly less naive packing routine that only allocates nodes,
+    but rotates round-robin style over the nodes to attempt a more
+    balanced allocation.'''  
+
     log.info("Packing by size, RR")
-    nodes = orig_nodes.copy()
-    allocated_vms = vms.copy()
+
+    nodes, vms = pack_setup(orig_nodes, orig_vms, vm_sort_key=key)
+    allocated_vms = []
+
+    while vms:
+
+        allocations = 0
+        node_index = 0
+        nodes_avail = len(nodes)
+
+        for vm in vms:
+            log.info("Attempt placing {}({:>.1f}GB,{} cpu) = {}".format(vm, vm.maxmem/2**30, vm.maxcpu, vm.area()))
+            allocated = False
+
+            for i in range(nodes_avail):
+
+                node_index += 1
+                node = nodes[node_index % nodes_avail]
+
+                log.info("  on {}:".format(node))
+
+                if node.allocate(vm):
+                    log.info("  Placed {} on {}".format(vm, node))
+                    allocations += 1
+                    allocated_vms.append(vm)
+                    allocated = True
+                    break
+
+            if not allocated:
+                log.info("  Failed to place {}".format(vm))
+
+        for vm in allocated_vms:
+            if vm in vms:
+                vms.remove(vm)
+
+        # if nothing was allocated, we're done, and break out of the
+        # outermost while loop
+        if allocations == 0:
+            break
+
+    if vms:
+        log.error("Failed to place {}  VMs! {}".format(len(vms), list(map(str, vms))))
+    else:
+        log.info("Successfully packed all {} VMs".format(len(orig_vms)))
+
+
+    # Print vms by node
+    for node in sorted(nodes):
+        print(node.name)
+        for vm in sorted(node.allocated_vms):
+            print('  {}'.format(vm))
+
+
+
+
+        
+
+            
 
     return nodes, len(allocated_vms), len(vms)
 
@@ -154,8 +215,48 @@ def pack_size_rr(orig_nodes, vms):
 # dimensions of the nodes and VMs.
 def pack_size_df(orig_nodes,vms):
     log.info("Packing by DF")
-    nodes = orig_nodes.copy()
-    allocated_vms = vms.copy()
+
+    nodes, vms = pack_setup(orig_nodes, orig_vms, vm_sort_key=key)
+    allocated_vms = []
+
+    while vms:
+
+        allocations = 0
 
 
     return nodes, len(allocated_vms), len(vms)
+
+
+def pack_skeleton(orig_nodes, orig_vms, key='area'):
+    '''Skeleton text about the packing routine.'''
+
+    log.info("Packing skeletons in closet.")
+
+    # Basic sorting and setup 
+    nodes, vms = pack_setup(orig_nodes, orig_vms, vm_sort_key=key)
+
+    # initially empty list of VMs that have been placed somewhere.
+    # if it isn't in this list, it wasn't placed.
+    allocated_vms = []
+
+    # Loop so long as there are vms to process
+    while vms:
+
+        # If allocations is zero at the end of this pass, 
+        # we've done nothing, and break out.
+        allocations = 0
+
+        # Sequentially loop over all VMs in the list.  This list
+        # was sorted in pack_setup() above, so no need to do it again.
+        for vm in vms:
+            log.info("Attempt placing {}({:>.1f}GB,{} cpu) = {}".format(vm, vm.maxmem/2**30, vm.maxcpu, vm.area()))
+            # Magic here
+
+            for node in nodes:
+                log.info("  on {}:".format(node))
+                # Magic here
+
+            pass
+
+
+    return None, None
